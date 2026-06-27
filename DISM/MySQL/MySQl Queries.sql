c12f01d2c4e3f9f2974c3fe658cac5a0f16342b9
farazinam <farazinam17@gmail.com>
2026-06-27 11:05:07 +0500

    DML Queries

--- DISM/MySQL/MySQl Queries.sql
+++ DISM/MySQL/MySQl Queries.sql
@@ -43,21 +43,66 @@ VALUES ("Hassan", 27, "Multan", "hassan@gmail.com");
 -- alter 
 
 -- Add Column
-
 ALTER TABLE product
 ADD COLUMN product_description INT;
 
+-- Change DataType
 ALTER TABLE product
 MODIFY COLUMN product_description VARCHAR (100);
 
+-- Delete Colun
 ALTER TABLE product
 DROP COLUMN product_desc;
 
+-- Rename Column
 ALTER TABLE product
 CHANGE product_description product_desc VARCHAR (100);
 
 SELECT * FROM product;
 
+-- day 3 ----------------
 
+INSERT INTO cust (name, age, city, email)
+VALUES ("Hasham", 21, "Murree", "hasham@gmail.com");
+
+-- Truncate
+TRUNCATE table cust;
+
+-- Drop
+DROP TABLE cust;
+DROP DATABASE ecommerce;
+
+-- Rename
+RENAME TABLE customer TO employee;
+ALTER TABLE employee RENAME TO customer;
+
+-- DML
+SET SQL_SAFE_UPDATES = 0;
+-- Update
+UPDATE customer SET age = 19;
+UPDATE customer SET age = 20 WHERE id = 8;
+UPDATE customer SET age = 19 WHERE name = "Ali";
+UPDATE customer SET name = "Ahsan" WHERE city = "Islamabad";
+UPDATE customer SET city = "Faisalabad" WHERE email is null;
+
+-- Delete
+DELETE from customer;  -- Delete all record from a table
+DELETE FROM customer WHERE id = 8;
+
+
+-- select
+SELECT * FROM customer;
+SELECT * FROM customer WHERE city = "Faisalabad";
+SELECT name FROM customer WHERE city = "Faisalabad";
+SELECT name, age FROM customer WHERE city = "Faisalabad";
+SELECT * FROM customer WHERE age < 20;
+SELECT * FROM customer WHERE email is null;
+SELECT * FROM customer WHERE email is not null;
+SELECT * FROM customer WHERE not email is null;
+
+SELECT * FROM customer LIMIT 3;
+
+SELECT name from customer;
+SELECT name, city from customer;
 
 
