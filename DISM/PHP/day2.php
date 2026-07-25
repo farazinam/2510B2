440b0f57eccff05f99d15c5274e0bd344103e99c
farazinam <farazinam17@gmail.com>
2026-07-25 11:36:59 +0500

    Basics

---
+++ DISM/PHP/day2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Welcome to HTML</h2>
+
+    <?php print "Welcome to PHP" ?>
+    <?php print("Welcome to PHP") ?>
+    <?php echo "Welcome to PHP" ?>
+    <?php echo("Welcome to PHP") ?>
+    <?php print "<h2> Welcome to PHP </h2>" ?>
+
+</body>
+</html>
+
+<?php
+    $num = 5;
+    echo $num;
+ ?>
+
+ <?= "Faraz Inam" ?>
